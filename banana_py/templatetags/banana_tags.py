91c7bd09cce044d584ea1db9d61329c170063611
Andrew Velis <avelis@users.noreply.github.com>
2015-12-03 19:36:37 -0600

    Addition of banana_auth_link.
    The separation of anchor tag HTML text from the url allows for a developer to custom the look and feel of the authorization flow to suit their needs.

--- banana_py/templatetags/banana_tags.py
+++ banana_py/templatetags/banana_tags.py
@@ -5,5 +5,9 @@ from banana_py import Bananas_OAuth
 register = template.Library()
 
 @register.simple_tag
-def banana_auth_url(link_text):
-    return u'<a href="%s">%s</a>' % (Bananas_OAuth().authorize_url(), link_text)
+def banana_auth_url():
+    return Bananas_OAuth().authorize_url()
+
+@register.simple_tag
+def banana_auth_link(link_text):
+    return u'<a href="%s">%s</a>' % (banana_auth_url(), link_text)
